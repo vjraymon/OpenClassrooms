80707a790495d2ff9268578f09595c15525184fe
vjraymon <49063544+vjraymon@users.noreply.github.com>
2021-06-14 07:36:57 +0200

    Step 2
    Create new activity "display neighbour" on click on a name in list neighbour activity
    Add a button "return" in activity  "display neighbour"
    Transmit an display the selected name in activity  "display neighbour"

--- Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/DisplayNeighbourActivity.java
+++ Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/DisplayNeighbourActivity.java
@@ -2,6 +2,7 @@ package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.openclassrooms.entrevoisins.R;
 
@@ -11,5 +12,9 @@ public class DisplayNeighbourActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_neighbour);
+
+        TextView displayName = findViewById(R.id.displayName);
+        String data = getIntent().getExtras().getString("keyName", "defaultKey");
+        displayName.setText(data);
     }
 }
--- Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourActivity.java
+++ Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.openclassrooms.entrevoisins.R;
 
@@ -46,8 +47,10 @@ public class ListNeighbourActivity extends AppCompatActivity {
         AddNeighbourActivity.navigate(this);
     }
 
-    public void sendMessage(View view){
+    public void displayNeighbour(View view){
         Intent intent = new Intent(this, DisplayNeighbourActivity.class);
+        TextView displayName = (TextView)view;
+        intent.putExtra("keyName", displayName.getText());
         startActivity(intent);
     }
 }
